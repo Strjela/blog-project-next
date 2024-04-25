84d3d86b6442681755a113c71c4f9105a7d82293
Strjela <nikola.jelic44@gmail.com>
2024-04-25 12:25:57 +0200

    a

---
+++ src/app/about/page.tsx
@@ -0,0 +1,25 @@
+import { headers } from "next/headers";
+import config from "../config";
+
+/* const fetchData = async () => {
+  const reqOptions = {
+    headers: {
+      Authorization: `Bearer ${process.env.API_TOKEN}`,
+    },
+  };
+  const request = await fetch(
+    `${config.api}/api/firsts?=populate=*`,
+    reqOptions
+  );
+  const response = await request.json();
+
+  return response;
+}; */
+
+const About = async () => {
+  /*   const data = await fetchData();
+  console.log(data.data[0].attributes.Description[0].children[0].text); */
+  return <h1>About</h1>;
+};
+
+export default About;
